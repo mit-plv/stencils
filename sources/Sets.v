48c693f4749d698b373d01057dfe8c12389cc4f6
Thomas Gregoire <thomas.gregoire@ens-lyon.fr>
2014-09-20 16:24:30 -0400

    proved seq_weak, tuned the set library

--- sources/Sets.v
+++ sources/Sets.v
@@ -22,6 +22,9 @@ Definition same {U} (A B : set U) :=
   forall x, x ∈ A <-> x ∈ B.
 Infix "≡" := same (at level 70, no associativity) : set_scope.
 
+Lemma mutual_inclusion : forall U (A B : set U), A ⊆ B -> B ⊆ A -> A ≡ B.
+Proof. intros; split; intuition. Qed.
+
 Definition empty (U : Type) : set U :=
   fun _ => False.
 Notation "∅" := (empty _) : set_scope.
@@ -36,8 +39,10 @@ Notation "⎨ x ; .. ; y ⎬" :=
   (finite (cons x .. (cons y nil) ..))
     (at level 0, x at next level, y at next level): set_scope.
 
-Inductive union {U} (A : set (set U)) : set U :=
-| union_ax : forall x y, x ∈ A -> y ∈ x -> y ∈ (union A).
+(*Inductive union {U} (A : set (set U)) : set U :=
+| union_ax : forall x y, x ∈ A -> y ∈ x -> y ∈ (union A).*)
+Definition union {U} (A : set (set U)) : set U :=
+  fun y => exists x, x ∈ A /\ y ∈ x.
 Notation "⋃ A" := (union A) (at level 35) : set_scope.
 Infix "∪" :=
   (fun A B => ⋃ ⎨A; B⎬) (at level 40, left associativity) : set_scope.
@@ -45,20 +50,26 @@ Infix "∪" :=
 Definition powerset {U} (B : set U) : set (set U) := fun A => A ⊆ B.
 Notation "'℘' A" := (powerset A) (at level 35) : set_scope.
 
-Inductive extension {U} (A : set U) (P : U -> Prop) : set U :=
-| extension_ax : forall x, x ∈ A -> P x -> x ∈ extension A P.
+(*Inductive extension {U} (A : set U) (P : U -> Prop) : set U :=
+| extension_ax : forall x, x ∈ A -> P x -> x ∈ extension A P.*)
+Definition extension {U} (A : set U) (P : U -> Prop) : set U :=
+  fun x => x ∈ A /\ P x.
 Notation "⎨ x ∈ A | P ⎬" :=
   (extension A (fun x => P))
     (at level 0, x at next level, A at next level, P at next level) : set_scope.
 
-Inductive image {U V} (A : set U) (f : U -> V) : set V :=
-| image_ax : forall x, x ∈ A -> f x ∈ image A f.
+(*Inductive image {U V} (A : set U) (f : U -> V) : set V :=
+| image_ax : forall x, x ∈ A -> f x ∈ image A f.*)
+Definition image {U V} (A : set U) (f : U -> V) : set V :=
+  fun y => exists x, x ∈ A /\ y = f x.
 Notation "⎨ e , x ∈ A ⎬" :=
   (image A (fun x => e))
     (at level 0, x at next level, A at next level, e at next level) : set_scope.
 
-Inductive times {U V} (A : set U) (B : set V) : set (U * V) :=
-| times_ax : forall x y, x ∈ A -> y ∈ B -> (x, y) ∈ times A B.
+(*Inductive times {U V} (A : set U) (B : set V) : set (U * V) :=
+| times_ax : forall x y, x ∈ A -> y ∈ B -> (x, y) ∈ times A B.*)
+Definition times {U V} (A : set U) (B : set V) : set (U * V) :=
+  fun p => fst p ∈ A /\ snd p ∈ B.
 Infix "×" := times (at level 39, left associativity) : set_scope.
 
 Definition segment (x y : nat) : set nat :=
--- sources/Stencils.v
+++ sources/Stencils.v
@@ -59,23 +59,19 @@ Section Refinement.
   Local Open Scope monoid.
 
   (** The actual spacetime into which the stencil code is executed. *)
-  
-  Definition sp : set (cell * time) :=
+
+  Definition group := set (cell * time).
+  Definition sp : group :=
     space ×〚 0, nb_iter-1 〛.
 
   (** Dependency relation between two cells in [real_space]. *)
-  
+
   Inductive neighbor : (cell * time) -> (cell * time) -> Prop :=
   | Neighb :
       forall (n c : cell) (t : time),
-        space (c + n) -> space (c + center) ->
+(*        (c + n) ∈ space -> (c + center) ∈ space ->*)
         In n pattern -> neighbor (c + n, t) (c + center, 1+t).
-
   Notation "c1 → c2" := (neighbor c1 c2) (at level 80).
-
-  (** XXX: Describe this system. *)
-  
-  Definition group := set (cell * time).
 
   (** One-step closure *)
 
@@ -96,7 +92,74 @@ Section Refinement.
   Definition closure (CC : set group) : set group :=
     ⋃ ⎨iter_next CC n, n ∈ ⟨nat⟩⎬.
 
+  (** XXX: Describe this system. *)
+
   Definition valid (A B : group) : Prop :=
     B ∈ closure ⎨A⎬.
+
+  (** XXX: I do not believe these proofs should be fully automated yet.  It
+   * would be nice to have a few tactics to make the process smoother, though.
+   *)
+
+  Lemma next_monotonic :
+    forall (CC DD : set group),
+      CC ⊆ DD -> next CC ⊆ next DD.
+  Proof.
+    intros.
+    unfold subset; intros.
+    unfold next in *.
+    destruct H0.
+    exists x0; intuition.
+    destruct H1.
+    exists x1; intuition.
+  Qed.
+
+  Lemma iter_in :
+    forall A CC, A ∈ CC -> forall k, iter_next ⎨A⎬ k ⊆ iter_next CC k.
+  Proof.
+    intros A CC H; induction k.
+    unfold iter_next, subset.
+    intros. destruct H0; subst.
+    intuition.
+    destruct H0.
+    simpl.
+    now apply next_monotonic.
+  Qed.
+
+  Lemma iter_morphism :
+    forall m n CC, iter_next CC (n + m) = iter_next (iter_next CC n) m.
+  Proof.
+    induction m.
+    simpl; intuition.
+    intros.
+    replace (n + S m)%nat with (S (n + m)) by omega; simpl.
+    now rewrite IHm.
+  Qed.
+
+  Lemma is_in_subset :
+    forall U x (A B : set U), x ∈ A -> A ⊆ B -> x ∈ B.
+  Proof. intuition. Qed.
+
+  Lemma seq_weak {A B C} : valid A B -> valid B C -> valid A C.
+  Proof.
+    intros Hb Hc.
+    unfold valid, closure.
+    destruct Hb as [? [[nb [? ?]] ?]]; subst.
+    destruct Hc as [? [[nc [? ?]] ?]]; subst.
+    exists (iter_next ⎨A⎬ (nb + nc)).
+    split. exists (nb + nc); intuition.
+    apply is_in_subset with (iter_next ⎨B⎬ nc).
+    assumption.
+    rewrite iter_morphism.
+    now apply iter_in.
+  Qed.
+
+  Lemma par {A B C} : valid A B -> valid A C -> valid A (B ∪ C).
+  Proof.
+  Admitted.
+
+  Lemma seq {A B C} : valid A B -> valid B C -> valid A (B ∪ C).
+  Proof.
+  Admitted.
 
 End Refinement.
